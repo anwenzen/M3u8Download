823f65236e5a8fe9d8cbcd9e1c638d0f8171a4b0
vnzen <vnzen@qq.com>
2020-05-09 11:58:10 +0800

    .

--- new_m3u8Download.py
+++ new_m3u8Download.py
@@ -15,7 +15,7 @@ class ThreadPoolExecutorWithQueueSizeLimit(ThreadPoolExecutor):
 
 
 class M3u8Download:
-    def __init__(self, m3u8_url, save_dir='new_video', max_workers=64, ce_verify=True):
+    def __init__(self, m3u8_url, save_dir='new_video', max_workers=64, ce_verify=False):
         self.m3u8_url = m3u8_url  # 完整的m3u8文件链接  如：M3U8_URL = "https://www.bilibili.com/example/index.m3u8"
         self.front_url = None
         self.save_dir = save_dir  # 保存m3u8的文件名夹  如：SAVE_DIR = "index"
@@ -28,11 +28,7 @@ class M3u8Download:
         self.start()
 
     def start(self):
-        print(f"""M3U8_URL = '{self.m3u8_url}'\nSAVE_DIR = '{os.getcwd()}/{self.save_dir}'""")
-        if not os.path.exists(f"./{self.save_dir}"):
-            os.mkdir(f"./{self.save_dir}")
-        if not os.path.exists(f"./{self.save_dir}/ts"):
-            os.mkdir(f"./{self.save_dir}/ts")
+        print(f"""M3U8_URL: {self.m3u8_url}\nSAVE_DIR: {os.getcwd()}/{self.save_dir}""")
         if not self.ce_verify:
             requests.packages.urllib3.disable_warnings()
         try:
@@ -41,7 +37,7 @@ class M3u8Download:
             print(f"获取失败:{self.m3u8_url}")
             return
         for times in range(0, 5):  # 最多重试 5 次
-            print(f"\n第 {times + 1} 次尝试中")
+            print(f"第 {times + 1} 次尝试中") if times==0 else print(f"\n第 {times + 1} 次尝试中")
             self.success_sum = 0
             self.fail_sum = 0
             with ThreadPoolExecutorWithQueueSizeLimit(self.max_workers) as pool:
@@ -61,14 +57,14 @@ class M3u8Download:
                     continue
                 elif re.search(r'^http', line) is not None:
                     self.m3u8_url = line
-                    print(f"SECOND_M3U8_URL = '{self.m3u8_url}")
+                    print(f"SECOND_M3U8_URL: {self.m3u8_url}")
                 elif re.search(r'^/', line) is not None:
                     self.m3u8_url = self.front_url + line
-                    print(f"SECOND_M3U8_URL = '{self.m3u8_url}")
+                    print(f"SECOND_M3U8_URL: {self.m3u8_url}")
                 else:
                     self.m3u8_url = self.m3u8_url.rsplit("/", 1)[0] + '/' + line
-                    print(f"SECOND_M3U8_URL = '{self.m3u8_url}")
-            print(f"SELECT_SECONDARY_M3U8_URL = '{self.m3u8_url}")
+                    print(f"SECOND_M3U8_URL: {self.m3u8_url}")
+            print(f"SELECT_SECONDARY_M3U8_URL: {self.m3u8_url}")
             self.get_m3u8_info(self.m3u8_url)
         else:
             m3u8_text_str = res.text
@@ -77,6 +73,10 @@ class M3u8Download:
         res.close()
 
     def get_ts_url(self, m3u8_text_str):
+        if not os.path.exists(f"./{self.save_dir}"):
+            os.mkdir(f"./{self.save_dir}")
+        if not os.path.exists(f"./{self.save_dir}/ts"):
+            os.mkdir(f"./{self.save_dir}/ts")
         new_m3u8_file = open(f"./{self.save_dir}/new_{self.save_dir}.m3u8", 'w')
         for line in m3u8_text_str.split('\n'):
             if "#" in line:
@@ -112,10 +112,9 @@ class M3u8Download:
                             if chunk:
                                 ts.write(chunk)
                     self.success_sum += 1
-                    print(f"\r下载进度：{self.success_sum}/{self.ts_sum}", end="")
+                    print(f"\r下载进度:{self.success_sum}/{self.ts_sum: <10}", end="")
                 else:
-                    pass
-                    print(f"\t下载./{self.save_dir}/ts/{save_ts_name}失败, [服务器响应码status_code!=200]")
+                    print(f"\n下载./{self.save_dir}/ts/{save_ts_name}失败, [服务器响应码status_code!=200]")
                 res.close()
             else:
                 self.success_sum += 1
@@ -124,7 +123,7 @@ class M3u8Download:
                 os.remove(f"./{self.save_dir}/ts/{save_ts_name}")
 #             print(f"\t下载./{self.save_dir}/ts/{save_ts_name}失败,连接或下载超时")
             self.fail_sum += 1
-            print(f"\r下载进度:{self.success_sum}/{self.ts_sum}\t\t失败数:{self.fail_sum}", end="")
+            print(f"\r下载进度:{self.success_sum}/{self.ts_sum: <10}失败数:{self.fail_sum}", end="")
 
     # 下载 .key 文件
     def download_key(self, key_line):
@@ -146,7 +145,7 @@ class M3u8Download:
         except Exception:
             if os.path.exists(f"./{self.save_dir}/ts/key.key"):
                 os.remove(f"./{self.save_dir}/ts/key.key")
-            print("\t如果你看见这个，证明 .key文件没有下载成功，并且这个视频属于加密的视频，请手动下载并改名，放在途径为 ./ts/key.key")
+            print(f"\t如果你看见这个，证明 .key文件没有下载成功，并且这个视频属于加密的视频，请手动下载并改名，放在途径为 ./{self.save_dir}/ts/key.key")
         new_line = f'{key_line.split(mid_part)[0]}URI="./ts/key.key"{key_line.split(mid_part)[-1]}\n'
         return new_line
 
@@ -155,21 +154,21 @@ class M3u8Download:
         input_file = f"./{self.save_dir}/new_{self.save_dir}.m3u8"
         output_file = f"./{self.save_dir}.mp4"
         cmd = f"ffmpeg -allowed_extensions ALL -i {input_file} -acodec copy -vcodec copy -f mp4 {output_file}"
-        print(f"{'合并开始':=^20}")
+        print(f"\n{'合并开始':=^30}")
         os.system(cmd)
         shutil.rmtree(f"./{self.save_dir}")  # 合并成功后，删除 .ts 文件
-        print(f"{'合并完成':=^20}")
+        print(f"{'合并完成':=^30}")
 
         
 if __name__ == "__main__":
-    M3U8_URL_LIST = [input("输入M3U8_URL，同时输入多个M3U8_URL时要用空格分开：").split(" ")]
-    SAVE_NAME = [input("输入SAVE_NAME，同时输入多个SAVE_NAME要用空格分开：").split(" ")]
+    M3U8_URL_LIST = input("输入M3U8_URL，同时输入多个M3U8_URL时要用空格分开：").split(" ")
+    SAVE_NAME = input("输入SAVE_NAME，同时输入多个SAVE_NAME要用空格分开：").split(" ")
     # 如果M3U8_URL的数量 ≠ SAVE_NAME的数量
     # 则只会使用第一个SAVE_NAME，并且会加尾数，方便(我自己)一次性下载一部电视剧
     START_NUMBER = 1
     for i in range(len(M3U8_URL_LIST)):
         M3u8Download(M3U8_URL_LIST[i], 
-                     f"{SAVE_NAME[0]}{START_NUMBER:02}" if len(SAVE_NAME) != len[M3U8_URL_LIST] else SAVE_NAME[i], 
-                     64, 
+                     SAVE_NAME[i] if len(M3U8_URL_LIST)==len(SAVE_NAME) else f"{SAVE_NAME[0]}{START_NUMBER:02}",
+                     64,
                      False)
         START_NUMBER += 1
